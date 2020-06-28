dbf713199fecbb6fdd93585784a04170a38412f1
Martin Achtermann <mdacht777@gmail.com>
2020-06-28 19:59:10 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,14 +1,12 @@
 require 'pry'
 class BankAccount
   attr_accessor :balance,:status
+  attr_reader :name
   def initialize(name)
     @name=name
     @balance=1000
     @status="open"
     puts self.name==name
     # binding.pry
-  end
-  def name
-    binding.pry
   end
 end
