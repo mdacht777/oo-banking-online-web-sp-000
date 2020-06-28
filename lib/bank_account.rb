5dd600bba0f8b425394182e138621ebdfe68f6e3
Martin Achtermann <mdacht777@gmail.com>
2020-06-28 19:57:58 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,14 @@
+require 'pry'
 class BankAccount
-
+  attr_accessor :balance,:status
+  def initialize(name)
+    @name=name
+    @balance=1000
+    @status="open"
+    puts self.name==name
+    # binding.pry
+  end
+  def name
+    binding.pry
+  end
 end
